984cb70b13398f712923407a404bae1aefd127f8
Lauren Plenger <laurenb08@gmail.com>
2020-12-28 21:39:20 -0800

    added sql seeds and index.js

---
+++ Assets/seeds.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR (30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department INT NOT NULL
+) CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+)
